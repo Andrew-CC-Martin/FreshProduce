db4dad16f08a67905e143c609447e1a6641ba251
stemshell <amartin262@gmail.com>
2018-07-11 16:40:01 +1000

    complete basic iteration of catalogue

--- client/src/components/Catalogue.js
+++ client/src/components/Catalogue.js
@@ -12,7 +12,7 @@ class Catalogue extends React.Component {
   }
 
   componentDidMount() {
-    const url = "https://rawgit.com/stemshell/ed489a4e0fe8703fab32fb31f2099654/raw/ef90ab56ae7cc8f6a4302cfc6b55a32da94c5c74/products.json"
+    const url = "https://rawgit.com/stemshell/ed489a4e0fe8703fab32fb31f2099654/raw/2bdde1a768c4563548e7193c3ccbcd65fc0920d7/products.json"
     
     fetch(url)
       .then(response => response.json())
@@ -30,14 +30,11 @@ class Catalogue extends React.Component {
     console.dir(this.state.products)
 
     return (
-      <div>
-      <h1>Hello World</h1>
-      <div className="products">
-        {this.state.products.map((product) => {
-          return <Product key={product.id} imgUrl={product.img_path} name={product.name}/>
-        })}
-      </div>
-      </div>
+        <div className="catalogue">
+          {this.state.products.map((product) => {
+            return <Product key={product.id} imgUrl={product.img_path} name={product.name} uom={product.uom} price={product.unit_sell_price} />
+          })}
+        </div>
     )
   }
 }
