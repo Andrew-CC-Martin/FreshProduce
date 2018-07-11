1b3adf3811b9fe58546d63e4870cc0b8fccb8f19
andresrgallo <andresgallo@hotmail.com>
2018-07-11 10:22:12 +1000

    Add code to manage profile/signin/logout appearance withtin app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,8 +2,9 @@ import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+const isLoggedIn = localStorage.jwtToken !== undefined ? true : false;
 // Last
-// console.log(localStorage)
+console.log(isLoggedIn)
 // this.logout = this.logout.bind(this)
 class Header extends Component {
   constructor() {
@@ -33,10 +34,14 @@ class Header extends Component {
         Menu
       </DropdownToggle>
       <DropdownMenu>
-        <DropdownItem header><Link to='/login'>Login</Link></DropdownItem>
-        <DropdownItem ><Link to='/register'>Register</Link></DropdownItem>
-        <DropdownItem divider />
         <DropdownItem ><Link to='/'>Home</Link></DropdownItem>
+        {!isLoggedIn ?
+              <DropdownItem header><Link to='/login'>Login</Link></DropdownItem> : ''}
+        {!isLoggedIn ?
+        <DropdownItem ><Link to='/register'>Register</Link></DropdownItem> : ''}
+        <DropdownItem divider />
+        {isLoggedIn ?
+            <DropdownItem ><Link to='/profile'>Profile</Link></DropdownItem> : ''}
       </DropdownMenu>
     </Dropdown>
 </div>
