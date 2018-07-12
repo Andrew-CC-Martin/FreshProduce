f7f865107e0081285cf369ab3133dc6ec232f6a0
andresrgallo <andresgallo@hotmail.com>
2018-07-12 22:16:32 +1000

    Fix bug with local storage and add tests to App component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders via Enzyme shallow without crashing', () => {
+  shallow(<App />)
+})
+
+it('renders via Enzyme mount without crashing', () => {
+  const wrapper = mount(<App />)
+  expect(wrapper.contains('Fresh Produce')).toBe(true)
+})
