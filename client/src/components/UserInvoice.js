522dd5238a26c8594f80b642e0cdb41a7cd785c5
andresrgallo <andresgallo@hotmail.com>
2018-07-16 16:51:34 +1000

    Fix some issues with Contact us component, Add User invoice component and backend route

--- client/src/components/UserInvoice.js
+++ client/src/components/UserInvoice.js
@@ -3,12 +3,11 @@ import {Form, FormGroup, Input, Label, Button } from 'reactstrap'
 import axios from 'axios';
 
 
-class  Contactus extends Component {
+class  UserInvoice extends Component {
     constructor () {
     super();
     this.state = { 
         name: '',
-        email: '',
         message: ''
     }
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -24,28 +23,21 @@ class  Contactus extends Component {
     async handleSubmit(e) {
         e.preventDefault();
         const { name, email, message } = this.state;
-        // const form = await axios.post(`${process.env.REACT_APP_API_URL}/user/form`, {
-        //     name,
-        //     email,
-        //     message
-        // })
+        const form = await axios.post(`${process.env.REACT_APP_API_URL}/user/inv`, {
+            name,
+            email,
+            message
+        })
     }
 
     render() { 
         return (
-            <Form onSubmit={this.handleSubmit} style={{width: '600px', marginLeft: '30%'}} >
+            <Form onSubmit={this.handleSubmit} style={{width: '600px', marginLeft: '20%'}} >
                 <FormGroup>
                     <Label for="name">Name</Label>
                     <Input
                         type="text"
                         name="name"
-                        onChange={this.handleChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input
-                        type="email"
-                        name="email"
                         onChange={this.handleChange} />
                 </FormGroup>
                 <FormGroup>
@@ -61,4 +53,4 @@ class  Contactus extends Component {
     }
 }
  
-export default Contactus;
+export default UserInvoice;
