732f09853f9d2de78e6c2360e6a2a902e8a19fef
ashma <ashma91@gmail.com>
2018-07-25 19:22:40 +1000

    make footer responsive

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,27 +4,21 @@ class Footer extends React.Component {
     render() {
         return (
         <div>
-            {/* <div className="footerheader">
-                <h1> Footer </h1>
-            </div> */}
-            <div className= "footercontent">
+            <div className="footerwrapper">
+                <div className= "footercontact">
+                    <div className="hours">
+                        <h5>
+                        Trading hours Mon-Thu: 8am – 6:30pm Fri: 7.30am-9pm Sat-Sun: 7.30am-6pm
+                        </h5>
+                    </div>
+                    <div className="contact">
+                        <h5> Shop MM07 level 2 Westfield Belconnen ACT 2617 Phone 02 6251 0366</h5>
+                    </div>
+                </div>
             <div>
-                <h5 className="hours">
-                Trading hours
-                Mon-Thu: 8am – 6:30pm
-                Fri: 7.30am-9pm
-                Sat-Sun: 7.30am-6pm
-                </h5>
+                <h6 className="copyright">©Straight A's, 2018 </h6>
             </div>
-            <div className="contact">
-              <h5> Shop MM07 level 2
-                Westfield Belconnen ACT 2617
-                Phone 02 6251 0366</h5>
             </div>
-            <div>
-              <h6 className="copyright">©Straight A's, 2018 </h6>
-            </div>
-           </div>
         </div>
         )
     }
